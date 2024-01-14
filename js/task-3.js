85e174727f391ede9eb1881df5f91b809bce7f3a
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-01-14 16:29:43 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,7 @@
-
+function getElementWidth(content, padding, border) {
+    const paddingValue = parseFloat(padding);
+    const borderValue = parseFloat(border);
+    const contentValue = parseFloat(content);
+    const totalWidth = contentValue + 2 * paddingValue + 2 * borderValue;
+    return totalWidth;
+}

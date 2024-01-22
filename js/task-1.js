9d0ec32ef53c3ecaa6c0638919bd9e596e27e8e3
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-01-22 19:46:09 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,11 @@
-makeTransaction(quantity, pricePerDroid, customerCredits) 
-totalPrice = quantity * pricePerDroid;
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+let totalPrice = quantity * pricePerDroid;
 if (customerCredits < totalPrice) {
   return "Insufficient funds";
 }
     else {
-    return "You ordered {quantity} droids worth {totalPrice} credits!";
+    return  `You ordered ${quantity} droids worth ${totalPrice} credits! `;
+}
 }
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
@@ -12,5 +13,4 @@ console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 300
 console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
 console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
 console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
-
 

17900fa4f4be3ff98b679895cf2cc6cfb82d119e
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-01-28 14:13:26 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,10 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-let totalPrice = quantity * pricePerDroid;
-if (customerCredits < totalPrice) {
-  return "Insufficient funds";
+function slugify(title) {
+  title = title.lower();
+  words = title.split();
+  slug = '-'.join(words);
+  return slug;
 }
-    else {
-    return  `You ordered ${quantity} droids worth ${totalPrice} credits! `;
-}
-}
-
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
-
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"

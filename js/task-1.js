abc1da5ae09003c2054fbe87e9c505c045dd813d
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-01-28 16:04:01 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
 function slugify(title) {
-  const slug = title.toLowerCase().split('').join('-')
+  const slug = title.toLowerCase().split('-')
   return slug;
 }
 console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"

b4fbbc99cf2537b9c4b2ef9346e157348a1a370b
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-01-29 19:59:10 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,9 @@
-function checkForSpam(message) {
-    const lowercasedMessage = message.toLowerCase(); // Перетворення всіх символів рядка в нижній регістр для порівняння
-    if (lowercasedMessage.includes('spam') || lowercasedMessage.includes('sale')) {
-        return true;
-    } else {
-        return false;
+function filterArray(numbers, value) {
+  let filteredArray = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      filteredArray.push(numbers[i]);
     }
+  }
+  return filteredArray;
 }
-console.log(checkForSpam("Buy now, great sale!")); // true
-console.log(checkForSpam("Low prices, SPAM discounts")); // true
-console.log(checkForSpam("Check out our latest products")); // false

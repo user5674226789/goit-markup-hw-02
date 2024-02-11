01f0163dc81a8919e955671bc09568a6c7b105c8
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-02-11 18:50:43 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,3 @@
-const profile = {
-  username: "Jacob",
-  playTime: 300,
-  changeUsername(newName) {
-    this.username = newName;
-  },
-  updatePlayTime(hours) {
-    this.playTime += hours;
-  },
-  getInfo() {
-    return `${this.username} has ${this.playTime} active hours!`;
-  }
+const sortByDescendingFriendCount = (users) => {
+  return users.sort((user1, user2) => user2.friends.length - user1.friends.length);
 };
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
-
-profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"

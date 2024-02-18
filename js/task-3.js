d2b728df2d38019e66a14c23b9821ed39300850e
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-02-18 09:37:52 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,32 @@
-const sortByDescendingFriendCount = (users) => {
-  return users.sort((user1, user2) => user2.friends.length - user1.friends.length);
-};
+class StringBuilder {
+  #value;
+
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="

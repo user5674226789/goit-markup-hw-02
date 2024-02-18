09bf4656573c97b4ab3fccbdabc593cc208cc645
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-02-18 09:37:06 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,31 @@
-const getUsersWithFriend = (users, friendName) => {
-  const filteredUsers = users.filter((user) => {
-    return user.friends.some((friend) => friend === friendName);
-  });
-  return filteredUsers;
-};
+class Storage {
+  #items;
+
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const index = this.#items.indexOf(itemToRemove);
+    if (index !== -1) {
+      this.#items.splice(index, 1);
+    }
+  }
+}
+
+const storage = new Storage(["Apple", "Banana", "Orange"]);
+console.log(storage.getItems()); // ["Apple", "Banana", "Orange"]
+
+storage.addItem("Mango");
+console.log(storage.getItems()); // ["Apple", "Banana", "Orange", "Mango"]
+
+storage.removeItem("Banana");
+console.log(storage.getItems()); // ["Apple", "Orange", "Mango"]

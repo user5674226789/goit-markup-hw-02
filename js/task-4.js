02da4270b0b6f67a13fcb3ca356d83aa9eb9bccc
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-02-25 14:44:17 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,5 +1,1 @@
-const getTotalBalanceByGender = (users, gender) => {
-  return users
-    .filter((user) => user.gender === gender)
-    .reduce((totalBalance, user) => totalBalance + user.balance, 0);
-};
+

1e859328f47a8ee4ccb829728e7307a68849fb29
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-02-25 14:45:08 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,11 @@
-const categoriesList = document.getElementById('categories');
-const categories = categoriesList.querySelectorAll('.item'); 
+const categoriesList = document.getElementById(".categories");
+const categories = categoriesList.querySelectorAll(".item"); 
 
 console.log(`Кількість категорій: ${categories.length}`); 
 
 categories.forEach(category => { 
-    const categoryName = category.querySelector('h2').textContent;
-    const categoryItems = category.querySelectorAll('ul li'); 
+    const categoryName = category.querySelector("h2").textContent;
+    const categoryItems = category.querySelectorAll("ul li"); 
     console.log(`Назва категорії: ${categoryName}`);
     console.log(`Кількість елементів у категорії "${categoryName}": ${categoryItems.length}`);
 });

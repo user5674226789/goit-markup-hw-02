ee901da8d81d0db9a8d1d86b26730ef259474fa7
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-02-25 14:41:40 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,11 @@
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+const categoriesList = document.getElementById('categories');
+const categories = categoriesList.querySelectorAll('.item'); 
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+console.log(`Кількість категорій: ${categories.length}`); 
+
+categories.forEach(category => { 
+    const categoryName = category.querySelector('h2').textContent;
+    const categoryItems = category.querySelectorAll('ul li'); 
+    console.log(`Назва категорії: ${categoryName}`);
+    console.log(`Кількість елементів у категорії "${categoryName}": ${categoryItems.length}`);
+});

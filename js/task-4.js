26f0d75f4614cd9553eb7cf9e1524caa5bb66320
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-02-25 16:07:50 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector('.login-form');
 
+  loginForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const emailValue = loginForm.elements.email.value.trim();
+    const passwordValue = loginForm.elements.password.value.trim();
+
+    if (emailValue === '' || passwordValue === '') {
+      alert('All form fields must be filled in');
+      return; 
+    }
+
+    const formData = {
+      email: emailValue,
+      password: passwordValue
+    };
+    
+    console.log(formData);
+
+    loginForm.reset();
+  });

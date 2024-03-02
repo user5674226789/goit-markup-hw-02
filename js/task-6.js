ef948a65f56603903216f4391c442f2ec835e7af
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-03-02 21:41:21 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,27 +1,22 @@
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
   }
-  
   function createBoxes(amount) {
     for (let i = 0; i < amount; i++) {
       const box = document.createElement('div');
       const size = 30 + i * 10; // Збільшення розміру на 10px для кожного нового бокса
-      box.style.width =  `${size}px `;
-      box.style.height =  `${size}px `; 
+      box.style.width =  `${size}px`;
+      box.style.height =  `${size}px`; 
       box.style.backgroundColor = getRandomHexColor();
       boxesContainer.appendChild(box);
     }
   }
-  
-  
   const input = document.querySelector('input');
   const createButton = document.querySelector('[data-create]');
   const destroyButton = document.querySelector('[data-destroy]');
   const boxesContainer = document.getElementById('boxes');
-  
   createButton.addEventListener('click', () => {
     const amount = Number(input.value);
-  
     if (amount >= 1 && amount <= 100) {
       createBoxes(amount);
       input.value = '';

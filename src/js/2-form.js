3612c5bbc882505eabe75d5a7be1d8493a48c878
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-03-16 17:32:25 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,37 +1,23 @@
+const KEY_MESSAGE = "feedback-form-state";
+const formRef = document.querySelector('form');
 
-const formSet = document.querySelector('.feedback-form');
-const inputinfo = formSet.elements.email;
-const areainfo = formSet.elements.message;
-let saveinfo = { email: '', message: '' };
-
-const parsedinfo = JSON.parse(localStorage.getItem('feedback-form-state'));
-
-if (parsedinfo !== null) {
-  areainfo.value = parsedinfo.message;
-  inputinfo.value = parsedinfo.email;
-
-  saveinfo = parsedinfo;
+formRef.addEventListener('input', addLocalStorage);
+function addLocalStorage() {
+    const objMessage = JSON.stringify({ email: formRef.elements.email.value, message: formRef.elements.message.value });
+    localStorage.setItem(KEY_MESSAGE, objMessage);    
 }
 
-formSet.addEventListener('input', event => {
-  const email = event.currentTarget.elements.email.value;
-  const message = event.currentTarget.elements.message.value;
-
-  saveinfo.email = email.trim();
-  saveinfo.message = message.trim();
-  localStorage.setItem('feedback-form-state', JSON.stringify(saveinfo));
+document.addEventListener('DOMContentLoaded', () => {
+    const objMessage = JSON.parse(localStorage.getItem(KEY_MESSAGE)) || {};
+    formRef.elements.email.value = objMessage.email || '';
+    formRef.elements.message.value = objMessage.message || '';
 });
 
-formSet.addEventListener('submit', evt => {
-  evt.preventDefault();
-
-  if (saveinfo.email.length == 0 || saveinfo.message.length == 0) {
-    console.log(`please fill all field`);
-  } else {
-    console.log(saveinfo);
-    localStorage.removeItem('feedback-form-state');
-    formSet.reset();
-    saveinfo.email = '';
-    saveinfo.message = '';
-  }
-});
+formRef.addEventListener('submit', removeLocalStorage);
+function removeLocalStorage(event) {
+    event.preventDefault(); 
+    console.log(JSON.parse(localStorage.getItem(KEY_MESSAGE)));
+    localStorage.removeItem(KEY_MESSAGE);
+    formRef.elements.email.value = '';
+    formRef.elements.message.value = '';
+}

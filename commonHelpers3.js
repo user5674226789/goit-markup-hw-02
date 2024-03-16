f19103ebd341109fde66d4fd3b516483060a855e
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-03-16 15:29:48 +0000

    Deploying to gh-pages from @ user5674226789/goit-markup-hw-01@af30560c844dd1e9dd8467dc6cfcc57f4ee415ca 🚀

--- commonHelpers3.js
+++
@@ -1,2 +0,0 @@
-import"./assets/styles-9f2083c0.js";const n=document.querySelectorAll(".linkList");n.forEach(e=>{e.addEventListener("click",t=>{t.preventDefault();const r=e.querySelector("a").getAttribute("href");window.location.href=r})});
-//# sourceMappingURL=commonHelpers3.js.map

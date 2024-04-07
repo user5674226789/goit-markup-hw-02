27c1f45b208a63032dfec5e8d9d253a912739ff0
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-04-07 21:25:23 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,80 +1,17 @@
-import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
-
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-
-let userSelectedDate;
-let timeInterval;
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    userSelectedDate = selectedDates[0];
-    timeInterval = userSelectedDate - options.defaultDate;
-
-    if (timeInterval < 0) {
-      iziToast.error({
-        color: 'red',
-        position: 'topRight',
-        message: `Please choose a date in the future`,
-      });
-    } else {
-      startBtn.disabled = false;
-      inputTime.disabled = true;
-    }
-  },
-};
-
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
+export function getImages(q) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const params = new URLSearchParams({
+    key: '43096972-1afb589163252f275da428b1d',
+    q,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
+  const url = `${BASE_URL}?${params}`;
+  return fetch(url)
+    .then(res => res.json())
+    .catch(error => {
+      console.error('Error occurred while fetching images:', error);
+      throw error;
+    });
 }
-
-const calendar = flatpickr('#datetime-picker', options);
-const inputTime = document.querySelector('#datetime-picker');
-const startBtn = document.querySelector('[data-start]');
-const timeValueElements = document.querySelectorAll('.value');
-
-
-console.log(timeValueElements);
-
-startBtn.disabled = true;
-
-startBtn.addEventListener('click', event => {
-  const repeatTime = setInterval(() => {
-    timeInterval = userSelectedDate - new Date();
-    inputTime.disabled = true;
-
-    if (timeInterval < 0) {
-      startBtn.disabled = true;
-      inputTime.disabled = false;
-      clearInterval(repeatTime);
-      return;
-    }
-
-    const timer = convertMs(timeInterval);
-
-    timeValueElements[0].textContent = timer.days.toString().padStart(2, '0');
-    timeValueElements[1].textContent = timer.hours.toString().padStart(2, '0');
-    timeValueElements[2].textContent = timer.minutes.toString().padStart(2, '0');
-    timeValueElements[3].textContent = timer.seconds.toString().padStart(2, '0');
-  }, 1000);
-});

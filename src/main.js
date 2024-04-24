8eba565730bacd6597406bfd5c919d52d0f836f6
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-04-24 08:04:08 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -75,6 +75,58 @@ function renderImages(images) {
     refs.imagesContainer.appendChild(imgElement);
   });
 }
+function handleImages(e) {
+  e.preventDefault();
+  load.classList.add('loader');
+  const input = e.target.elements.search.value.trim();
+  if (input === '') {
+    iziToast.error({
+      backgroundColor: 'lightred',
+      icon: false,
+      progressBar: false,
+      close: false,
+      position: 'topRight',
+      message: 'Please, fill the field!',
+    });
+    return;
+  }
+
+  getImages(input)
+    .then(value => {
+      if (value.hits && value.hits.length === 0) {
+        iziToast.error({
+          backgroundColor: 'red',
+          progressBar: false,
+          close: false,
+          position: 'topRight',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      }
+
+      const markup = renderImages(value.hits);
+      list.innerHTML = markup;
+      const lightbox = new SimpleLightbox('.gallery-link', {
+        captionsData: 'alt',
+      });
+      lightbox.refresh();
+    })
+    .catch(error => {
+      console.error('Error occurred while fetching images:', error);
+      iziToast.error({
+        backgroundColor: 'red',
+        icon: false,
+        progressBar: false,
+        close: false,
+        position: 'topRight',
+        message:
+          'Sorry, an error occurred while fetching images. Please try again!',
+      });
+    })
+    .finally(() => load.classList.remove('loader'));
+  e.target.reset();
+}
+
 
 refs.btnShowMore.addEventListener('click', onLoadMoreClick);
 

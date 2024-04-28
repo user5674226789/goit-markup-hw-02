c2200bd3adba96f166a06e1bd513744fa93d779c
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-04-28 15:42:07 +0300

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,45 +1,32 @@
-import { gallery } from '../main';
+import iziToast from 'izitoast';
+import { refs, lightbox } from '../main';
+import 'izitoast/dist/css/iziToast.min.css';
 
-export function renderGallery(data) {
-  const createMarkup = data.hits
-    .map(hit => {
-      const {
-        id,
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      } = hit;
-      return `
-    <li class="gallery-item">
-        <a class="gallery-link" href="${largeImageURL}">
-          <img class="gallery-image" id=${id} src="${webformatURL}" alt="${tags}"/>
-        </a>
-        <div class="gallery-item-info">
-          <div class="item-info-atr">
-          <h3>Likes</h3>
-          <p>${likes}</p>
-          </div>
-          <div class="item-info-atr">
-          <h3>Views</h3>
-          <p>${views}</p>
-          </div>
-          <div class="item-info-atr">
-          <h3>Comments</h3>
-          <p>${comments}</p>
-          </div>
-          <div class="item-info-atr">
-          <h3>Downloads</h3>
-          <p>${downloads}</p>
-          </div>
-        </div>
-    </li>
-    `;
-    })
-    .join('');
-
-  gallery.insertAdjacentHTML('beforeend', createMarkup);
+export function renderImg(arr) {
+  if (arr.length === 0) {
+    iziToast.error({
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
+      position: 'topRight',
+    });
+  } else {
+    const markup = arr
+      .map(
+        photo =>
+          `<li class="photos-block">
+            <a class="photos-link" href="${photo.largeImageURL}">
+            <img class="photo" src="${photo.webformatURL}" alt="${photo.tags}"/>
+            </a>
+            <ul class="photo-data">
+            <li class="photo-data-detail"><p><span class="info">Likes</span></br>${photo.likes}</p></li>
+            <li class="photo-data-detail"><p><span class="info">Views</span></br>${photo.views}</p></li>
+            <li class="photo-data-detail"><p><span class="info">Comments</span></br>${photo.comments}</p></li>
+            <li class="photo-data-detail"><p><span class="info">Downloads</span></br>${photo.downloads}</p></li>
+            </ul>
+            </li>`
+      )
+      .join('');
+    refs.galleryList.insertAdjacentHTML('beforeend', markup);
+    lightbox.refresh();
+  }
 }

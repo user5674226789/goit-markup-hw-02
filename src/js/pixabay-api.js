7f1e6ec980f05dffbe17ad6ea5749c0014672d42
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-04-28 16:05:35 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,16 +1,23 @@
-export function getImages(q) {
-  const BASE_URL = 'https://pixabay.com/api/';
-  const params = new URLSearchParams({
-    key: '43096972-1afb589163252f275da428b1d',
-    q,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-  });
-  const url = `${BASE_URL}?${params}`;
-  return fetch(url)
-    .then(res => res.json())
-    .catch(error => {
-      console.error('Error occurred while fetching images:', error);
-      throw error;
-    });
+import axios from "axios";
+import { refs, query, showLoader, currentPage, pageLimit} from "../main";
+
+const myAxios = axios.create ({
+    baseURL: "https://pixabay.com/api/",
+})
+
+export async function getImages() {
+    const params = {
+        key: "43015885-c3997a91f23585149f1baaf72",
+        q: query,
+        per_page: pageLimit,
+        page: currentPage,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: "true",
+    };
+
+    showLoader();
+
+    const response = await myAxios.get("", { params })
+    return response.data;
+};

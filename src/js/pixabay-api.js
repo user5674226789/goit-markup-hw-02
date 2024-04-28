98f90ebf588c67884edbe0c9bebddf3340831843
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-04-28 15:37:31 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,21 +1,26 @@
-import axios from "axios";
+import axios from 'axios';
+import iziToast from 'izitoast';
 
-export async function getImages(query, currentPage) {
+export async function fetchImages(searchQueryResult, currentPage) {
+  try {
     const BASE_URL = 'https://pixabay.com/api/';
-    const params = {
-      key: '43180958-6b06a0884e2ebb29e4f1cb8bf',
-      q: query,
+    const q = searchQueryResult;
+    const params = new URLSearchParams({
+      key: '42946583-b9bd64904b8b2d582b051d84e',
+      q: q,
       image_type: 'photo',
       orientation: 'horizontal',
-      safesearch: 'true',
+      safesearch: true,
       per_page: 15,
       page: currentPage,
-    };
-    try {
-      const res = await axios.get(BASE_URL, {params});
-      return res.data
-    } catch (error) {
-        console.error('Error occurred while fetching images:', error);
-        throw error;
-      };
+    });
+    const url = `${BASE_URL}?${params}`;
+    const res = await axios.get(url);
+    return res.data;
+  } catch (error) {
+    iziToast.error({
+      message: 'Server error!',
+      position: 'topRight',
+    });
   }
+}
